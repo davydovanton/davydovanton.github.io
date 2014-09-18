c9d6175f5e00e5b8281ca76cf1a6ee9587222b79
Anton Davydov <don.mist@gmail.com>
2014-09-19 03:19:24 +0400

    Add pure menu + stasting redesign site

--- _site/Gemfile
+++ _site/Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'emoji_for_jekyll'
+gem 'jekyll-sass-converter'
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+$(window).scroll(function(){
+  $('.parallax').css('background-position','100%'+($(window).scrollTop()*0.3)+'px');
+});
+
+function EasyPeasyParallax() {
+	scrollPos = $(this).scrollTop();
+	$('#banner').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+	$('#bannertext').css({
+		'margin-top': (scrollPos/4)+"px",
+		'opacity': 1-(scrollPos/250)
+	});
+}
+$(document).ready(function(){
+	$(window).scroll(function() {
+		EasyPeasyParallax();
+	});
+});
